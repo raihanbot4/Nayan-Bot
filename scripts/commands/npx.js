bdd9771b0ac13063bc1750dd50fa3721a14bff59
raihanbot4 <raihansaiko@gmail.com>
2024-08-10 10:59:12 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "😒",
+	name: "raihan",
         version: "1.0.1",
         prefix: false,
 	permssion: 0,
 	credits: "nayan", 
 	description: "Fun",
 	category: "no prefix",
-	usages: "😒",
+	usages: "raihan",
         cooldowns: 5, 
 },
 
@@ -16,13 +16,13 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😒")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf("রায়হান")==0 || body.indexOf("raihan")==0 || body.indexOf("Raihan")==0) {
 		var msg = {
-				body: "এঁভাঁবেঁ তাঁকাঁসঁ নাঁ প্রেঁমেঁ পঁরেঁ যাঁবোঁ 😚🥀",
-				attachment: fs.createReadStream(__dirname + `/Nayan/Mayabi.mp3`)
+				body: " __রায়হান জানু তাহ এখন বিজি আছে..!!🐰🍒",
+				attachment: fs.createReadStream(__dirname + `/cache/janu.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😁", event.messageID, (err) => {}, true)
+    api.setMessageReaction("💋", event.messageID, (err) => {}, true)
 		}
 	},
 	start: function({ nayan }) {
